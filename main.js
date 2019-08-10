d66019944741f54db2998fc894071145589c79a1
Ibrahim Yunus <younis.ib@gmail.com>
2019-08-10 11:42:45 -0400

    added main.js link to the index.html file, added api calls

--- main.js
+++ main.js
@@ -1,102 +1,59 @@
-     // Initial array of movies
-     var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+$(function() {
 
-     // displayMovieInfo function re-renders the HTML to display the appropriate content
-     function displayMovieInfo() {
+    // Get data from UTELY API via RAPID KEY
+    let apiUrl = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?country=us&term=";
+    let searchTerm = "avengers";
+    let rapidHost = "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com";
+    let rapidKey = "e8c18e9a6emsh93df675062d03fdp10e88bjsn4870cb0d0bec";
 
-         var movie = $(this).attr("data-name");
-         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+    $.get({
+        url: apiUrl + searchTerm,
+        dataType: 'json',
+        headers: {
+            "x-rapidapi-host": rapidHost,
+            "x-rapidapi-key": rapidKey
 
-         // Creating an AJAX call for the specific movie button being clicked
-         $.ajax({
-             url: queryURL,
-             method: "GET"
-         }).then(function (response) {
+        }
+    }).then(function(response) {
 
-             // Creating a div to hold the movie
-             var movieDiv = $("<div class='movie'>");
+        let utelyDatas = response.results;
+        console.log("------------ Utely --------")
+        console.log(utelyDatas);
+    });
 
-             // Storing the rating data
-             var rating = response.Rated;
 
-             // Creating an element to have the rating displayed
-             var pOne = $("<p>").text("Rating: " + rating);
+    // Get data from OMD database
+    let movie = "The Matrix";
+    let queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy";
 
-             // Displaying the rating
-             movieDiv.append(pOne);
+    $.get(queryURL)
+        .then(function(response) {
 
-             // Storing the release year
-             var released = response.Released;
+            let omDB = response.Search;
+            console.log("------------ OMDB --------")
+            console.log(omDB);
 
-             // Creating an element to hold the release year
-             var pTwo = $("<p>").text("Released: " + released);
+        });
 
-             // Displaying the release year
-             movieDiv.append(pTwo);
 
-             // Storing the plot
-             var plot = response.Plot;
+    // Get movie information from The Movie DB
 
-             // Creating an element to hold the plot
-             var pThree = $("<p>").text("Plot: " + plot);
+    let keyApi = "fa797fcbd4bd5cb308e4eaaae9007e07";
 
-             // Appending the plot
-             movieDiv.append(pThree);
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/movie/now_playing?api_key=" + keyApi + "&language=en-US&page=1",
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    }
 
-             // Retrieving the URL for the image
-             var imgURL = response.Poster;
+    $.ajax(settings).done(function(response) {
 
-             // Creating an element to hold the image
-             var image = $("<img>").attr("src", imgURL);
+        console.log("------------The Movie BD --------")
+        console.log(response.results);
+    });
 
-             // Appending the image
-             movieDiv.append(image);
 
-             // Putting the entire movie above the previous movies
-             $("#movies-view").prepend(movieDiv);
-         });
-
-     }
-
-     // Function for displaying movie data
-     function renderButtons() {
-
-         // Deleting the movies prior to adding new movies
-         // (this is necessary otherwise you will have repeat buttons)
-         $("#buttons-view").empty();
-
-         // Looping through the array of movies
-         for (var i = 0; i < movies.length; i++) {
-
-             // Then dynamicaly generating buttons for each movie in the array
-             // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-             var a = $("<button>");
-             // Adding a class of movie-btn to our button
-             a.addClass("movie-btn");
-             // Adding a data-attribute
-             a.attr("data-name", movies[i]);
-             // Providing the initial button text
-             a.text(movies[i]);
-             // Adding the button to the buttons-view div
-             $("#buttons-view").append(a);
-         }
-     }
-
-     // This function handles events where a movie button is clicked
-     $("#add-movie").on("click", function (event) {
-         event.preventDefault();
-         // This line grabs the input from the textbox
-         var movie = $("#movie-input").val().trim();
-
-         // Adding movie from the textbox to our array
-         movies.push(movie);
-
-         // Calling renderButtons which handles the processing of our movie array
-         renderButtons();
-     });
-
-     // Adding a click event listener to all elements with a class of "movie-btn"
-     $(document).on("click", ".movie-btn", displayMovieInfo);
-
-     // Calling the renderButtons function to display the intial buttons
-     renderButtons();
+});
