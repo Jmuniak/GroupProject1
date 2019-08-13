640b9195b14807a4c420a53396b19fe6e529209a
Ibrahim Yunus <younis.ib@gmail.com>
2019-08-12 20:49:30 -0400

    got the result from the user input on utely & IMDB API

--- main.js
+++ main.js
@@ -1,109 +1,126 @@
-$(function () {
+$(function() {
+
+    let $searchUtely = $("#searchUtely");
+
+    /* Sub
+    ======================================================================= */
+
+    $searchUtely.submit(function(event) {
+
+        event.preventDefault();
+
+        let $searchInput = $("#utelyInput").val().trim();
+
+        let searchValue = $searchInput;
 
 
-    /* Click Event
-     ======================================================================= */
+        utellyApiCall(searchValue);
+        iMDBApiCall(searchValue);
+    })
+
+    // function to get movie streaming service
+    let utellyApiCall = function(searchTerm) {
+
+        let apiUrlUtelly = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?country=us&term=";
+        let rapidHost = "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com";
+        let rapidKey = "e8c18e9a6emsh93df675062d03fdp10e88bjsn4870cb0d0bec";
 
 
+        $.get({
+            url: apiUrlUtelly + searchTerm,
+            dataType: 'json',
+            headers: {
+                "x-rapidapi-host": rapidHost,
+                "x-rapidapi-key": rapidKey
 
+            }
+        }).then(function(response) {
 
-
-
-    // Get data from UTELY API via RAPID KEY
-    let apiUrl = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?country=us&term=";
-    let searchTerm = "avengers";
-    let rapidHost = "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com";
-    let rapidKey = "e8c18e9a6emsh93df675062d03fdp10e88bjsn4870cb0d0bec";
-
-    $.get({
-        url: apiUrl + searchTerm,
-        dataType: 'json',
-        headers: {
-            "x-rapidapi-host": rapidHost,
-            "x-rapidapi-key": rapidKey
-
-        }
-    }).then(function (response) {
-
-        let utelyDatas = response.results;
-        console.log("------------ Utely --------")
-        console.log(utelyDatas);
-    });
-
-
-    // Get data from OMD database
-    let movie = "The Matrix";
-    let queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy";
-
-    $.get(queryURL)
-        .then(function (response) {
-
-            let omDB = response.Search;
-            console.log("------------ OMDB --------")
-            console.log(omDB);
-
+            let utelyDatas = response.results;
+            console.log("------------ Utely --------")
+            console.log(utelyDatas);
         });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Get movie information from The Movie DB
-
-    let keyApi = "fa797fcbd4bd5cb308e4eaaae9007e07";
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.themoviedb.org/3/movie/now_playing?api_key=" + keyApi + "&language=en-US&page=1",
-        "method": "GET",
-        "headers": {},
-        "data": "{}"
     }
 
-    $.ajax(settings).done(function (response) {
-
-        console.log("------------The Movie BD --------")
-        console.log(response.results);
-    });
+    let iMDBApiCall = function(searchTerm) {
 
 
-    // Get Movie by title 
+        let apiUrliMDB = "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&s=";
+        let hostIMDB = "movie-database-imdb-alternative.p.rapidapi.com";
+        let apiKeyIMDB = "e8c18e9a6emsh93df675062d03fdp10e88bjsn4870cb0d0bec";
 
-    let urlHost = "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&s=";
-    let superHero = "Batman";
-    let hostIMDB = "movie-database-imdb-alternative.p.rapidapi.com";
-    let apiKeyIMDB = "e8c18e9a6emsh93df675062d03fdp10e88bjsn4870cb0d0bec";
+        $.get({
+            url: apiUrliMDB + searchTerm,
+            dataType: 'json',
+            headers: {
+                "x-rapidapi-host": hostIMDB,
+                "x-rapidapi-key": apiKeyIMDB
 
-    $.get({
-        url: urlHost + superHero,
-        dataType: 'json',
-        headers: {
-            "x-rapidapi-host": hostIMDB,
-            "x-rapidapi-key": apiKeyIMDB
+            }
+        }).then(function(response) {
 
-        }
-    }).then(function (response) {
+            let dataIMDB = response.Search;
+            console.log("------------ IMDB --------")
+            console.log(dataIMDB);
+        });
+    }
 
-        let dataIMDB = response.Search;
-        console.log("------------ IMDB --------")
-        console.log(dataIMDB);
-    });
+    // // Get data from OMD database
+    // let movie = "The Matrix";
+    // let queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy";
+
+    // $.get(queryURL)
+    //     .then(function(response) {
+
+    //         let omDB = response.Search;
+    //         console.log("------------ OMDB --------")
+    //         console.log(omDB);
+
+    //     });
+
+
+    // // Get movie information from The Movie DB
+
+    // let keyApi = "fa797fcbd4bd5cb308e4eaaae9007e07";
+
+    // var settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://api.themoviedb.org/3/movie/now_playing?api_key=" + keyApi + "&language=en-US&page=1",
+    //     "method": "GET",
+    //     "headers": {},
+    //     "data": "{}"
+    // }
+
+    // $.ajax(settings).done(function(response) {
+
+    //     console.log("------------The Movie BD --------")
+    //     console.log(response.results);
+    // });
+
+
+    // // Get Movie by title 
+
+    // let urlHost = "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&s=";
+    // let superHero = "Batman";
+    // let hostIMDB = "movie-database-imdb-alternative.p.rapidapi.com";
+    // let apiKeyIMDB = "e8c18e9a6emsh93df675062d03fdp10e88bjsn4870cb0d0bec";
+
+    // $.get({
+    //     url: urlHost + superHero,
+    //     dataType: 'json',
+    //     headers: {
+    //         "x-rapidapi-host": hostIMDB,
+    //         "x-rapidapi-key": apiKeyIMDB
+
+    //     }
+    // }).then(function(response) {
+
+    //     let dataIMDB = response.Search;
+    //     console.log("------------ IMDB --------")
+    //     console.log(dataIMDB);
+    // });
 
 
 
