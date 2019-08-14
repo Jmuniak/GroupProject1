82f7bbbf8de19141a878ed1f6ff6bae3fae6666d
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-08-14 17:44:00 -0400

    Guidebox functional, ready for dynamic html styling.

--- main.js
+++ main.js
@@ -7,6 +7,32 @@ $(function () {
 
     $resultMessage.hide();
 
+
+    $("#gboxForm").submit(function (event) {
+        event.preventDefault();
+        console.log('submitted the form');
+        let gboxSearch = $("#gboxSearch").val();
+        console.log(gboxSearch);
+        let GBOX_API_KEY = "7cbaa5da2a59678a995910c255de77709361f8bd";
+        // for title search /v2/search?api_key=YOUR_API_KEY&type=movie&field=title&query=Terminator(gboxSearch)
+        // for shows search /v2/search?api_key=YOUR_API_KEY&type=show&field=title&query=Terminator(gboxSearch)
+        // for person search /v2/search?api_key=YOUR_API_KEY&type=person&query=Harrison+Ford
+        // I think we 
+        let gboxTitleSearchURL = "http://api-public.guidebox.com/v2/search?api_key=" + GBOX_API_KEY + "&type=movie&field=title&query=" + gboxSearch;
+        console.log("ajax start");
+        $.get({
+            url: gboxTitleSearchURL,
+            dataType: 'json',
+        })
+            .then(function (response) {
+                console.log(response);
+                let dataGBOX = response.data;
+                console.log("------------ GBOX --------")
+                console.log(dataGBOX);
+            })
+        console.log("ajax done");
+    });
+
     /* Event when the Form is submited
     ======================================================================= */
 
@@ -25,6 +51,8 @@ $(function () {
 
     /* Functions
     ======================================================================= */
+
+
 
     // function to get movie streaming service
     let utellyApiCall = function (searchTerm) {
